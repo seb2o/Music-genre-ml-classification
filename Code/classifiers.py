e72da2dd61403580b421e4dadc04522753fb7d2d
seb2o <87731987+seb2o@users.noreply.github.com>
2024-04-13 14:34:10 +0200

    added plotting of random forest feature importance

--- Code/classifiers.py
+++ Code/classifiers.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from tensorflow.keras import layers, models, callbacks
+
+import utils
 
 
 def tensorflow_fcnn(X_train: pd.DataFrame, y_train: pd.DataFrame, X_val: pd.DataFrame,
@@ -43,13 +46,11 @@ def tensorflow_fcnn(X_train: pd.DataFrame, y_train: pd.DataFrame, X_val: pd.Data
 
 
 def sklearn_rf(X_train: pd.DataFrame, y_train: pd.DataFrame, X_val: pd.DataFrame, y_val: pd.DataFrame):
-    rf_classifier = RandomForestClassifier(n_estimators=1000, random_state=42, max_depth=5)
-
-    # Train the classifier
+    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42, max_depth=10)
     rf_classifier.fit(X_train, y_train)
-
-    # Make predictions
-    y_train_pred = rf_classifier.predict(X_train)
     y_val_pred = rf_classifier.predict(X_val)
+    print(f'Accuracy : {np.mean(y_val == y_val_pred)}\n')
+    feature_importance_sort = rf_classifier.feature_importances_.argsort()
+    plt.barh(X_train.columns[feature_importance_sort][-20:], rf_classifier.feature_importances_[feature_importance_sort][-20:])
 
     return y_val_pred
